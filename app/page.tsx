ae3577e03d751a6330a94c426e645315a8675272
Gabriel Fernandez <gafernan@sfu.ca>
2024-06-20 14:22:34 -0700

    Added some description text
    Signed-off-by: Gabriel Fernandez <gafernan@sfu.ca>

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-    {/* <main className="flex min-h-screen flex-col items-center p-24 bg-header-background bg-top bg-[length:100%] bg-no-repeat"> */}
+      {/* <main className="flex min-h-screen flex-col items-center p-24 bg-header-background bg-top bg-[length:100%] bg-no-repeat"> */}
 
       <div className="mb-32 grid items-center text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <img className="justify-end" src="../myLogo-GabrielFernandez.png">
@@ -12,7 +12,7 @@ export default function Home() {
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           rel="noopener noreferrer"
           href="#about_me"
-          >
+        >
           <h2 className="text-2xl font-semibold">
             About Me{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
@@ -25,7 +25,7 @@ export default function Home() {
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           rel="noopener noreferrer"
           href="#work_experience"
-          >
+        >
           <h2 className="text-2xl font-semibold">
             Experience{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
@@ -38,7 +38,7 @@ export default function Home() {
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           rel="noopener noreferrer"
           href="#projects"
-          >
+        >
           <h2 className="text-2xl font-semibold">
             Projects{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
@@ -58,12 +58,12 @@ export default function Home() {
             Email: gabe.fer2000@gmail.com
           </li>
           <li>
-            <a href="https://www.linkedin.com/in/gabriel-antonio-fernandez-2201841a3/">
+            <a href="https://www.linkedin.com/in/gabriel-antonio-fernandez-2201841a3/" className="text-sky-700">
               LinkedIn
             </a>
           </li>
           <li>
-            <a href="https://github.com/GabeFernandez310">
+            <a href="https://github.com/GabeFernandez310" className="text-sky-700">
               GitHub
             </a>
           </li>
@@ -71,10 +71,10 @@ export default function Home() {
       </div>
 
       <img
-          className="mt-8 mb-8 size-2/3 center"
-          src="/GabrielFernandez-Hike.png"
-        />
-      
+        className="mt-8 mb-8 size-2/3 center"
+        src="/GabrielFernandez-Hike.png"
+      />
+
 
       <section id="about_me" className="mt-16 mb-32 text-left lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <h3 className="text-xl font-bold">
@@ -86,10 +86,8 @@ export default function Home() {
             Hi! If you&apos;re reading this, welcome to my personal page. My name is Gabriel (although more often I go by Gio), and I&apos;m a Software Developer based around Vancouver B.C., Canada.
             This page is still fairly barebones as I&apos;m actively working on it, but more information about me will come soon!
 
-            <br>
-            </br>
-            <br>
-            </br>
+            <br></br>
+            <br></br>
 
             Following this is some placeholder text... What is Lorem Ipsum?
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
@@ -121,23 +119,29 @@ export default function Home() {
             Software Developer I
           </h2>
           <p className="m-0 text-sm opacity-50">
+            Improving (Formerly Bit Quill Technologies), Vancouver, BC
+            <br></br>
+            <br></br>
+
             placeholder description.....Following this is some placeholder text... What is Lorem Ipsum?
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 
-
           </p>
-            <h3 className="mt-2">Key Responsibilities</h3>
-            <ul className="ml-8 mt-2 list-disc">
-              <li>
-                list item 1
-              </li>
-              <li>
-                list item 2
-              </li>
-              <li>
-                list item 3
-              </li>
-            </ul>
+          <h3 className="mt-2">Key Responsibilities</h3>
+          <ul className="ml-8 mt-2 list-disc">
+            <li>
+              Implemented new software features by adhering to client requirements to improve software functionality
+            </li>
+            <li>
+              Communicated with customers to inform them about project progress and discuss urgent topics
+            </li>
+            <li>
+              Trained colleagues by helping with development environment setup and tasks to increase team performance.
+            </li>
+            <li>
+              Discussed task progress and blocking issues with my team during daily meetings to receive help and feedback
+            </li>
+          </ul>
         </div>
 
         <div className="ml-4 mt-4">
@@ -145,23 +149,36 @@ export default function Home() {
             Quality Assurance Engineer
           </h2>
           <p className="m-0 text-sm opacity-50">
+
+            Alpha International, New Westminster, BC
+
+            <br></br>
+            <br></br>
+
             placeholder description.....Following this is some placeholder text... What is Lorem Ipsum?
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 
 
           </p>
-            <h3 className="mt-2">Key Responsibilities</h3>
-            <ul className="ml-8 mt-2 list-disc">
-              <li>
-                list item 1
-              </li>
-              <li>
-                list item 2
-              </li>
-              <li>
-                list item 3
-              </li>
-            </ul>
+          <h3 className="mt-2">Key Responsibilities</h3>
+          <ul className="ml-8 mt-2 list-disc">
+            <li>
+              Attended daily Stand-up meetings to communicate with my team and identify tasks and blockers
+
+            </li>
+            <li>
+              Created API tests in Postman to quickly identify bugs in the API and ease application deployment
+              list item 2
+            </li>
+            <li>
+              list item 3
+              Developed automated tests in Katalon to limit the amount of manual testing needed for the application
+            </li>
+            <li>
+              Created bug tickets in Jira to give detailed descriptions of identified issues that required attention
+
+            </li>
+          </ul>
         </div>
       </section>
 
@@ -172,26 +189,33 @@ export default function Home() {
 
         <div className="ml-4 mt-4">
           <h2 className="text-xl">
-            OpernSearch SQL Plugin
+            OpenSearch SQL Plugin
           </h2>
           <p className="m-0 text-sm opacity-50">
+
+            Open Source Project
+
+            <br></br>
+            <br></br>
+
             placeholder description.....Following this is some placeholder text... What is Lorem Ipsum?
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 
           </p>
 
-            <h3 className="mt-2">Key Responsibilities</h3>
-            <ul className="ml-8 mt-2 list-disc">
-              <li>
-                list item 1
-              </li>
-              <li>
-                list item 2
-              </li>
-              <li>
-                list item 3
-              </li>
-            </ul>
+          <h3 className="mt-2">Key Responsibilities</h3>
+          <ul className="ml-8 mt-2 list-disc">
+            <li>
+              Implemented software features according to community needs to match and improve upon legacy functionality
+
+            </li>
+            <li>
+              Addressed user issues and bugs by investigating and proposing solutions that satisfied given requirements
+            </li>
+            <li>
+              Used source control to review code, resolve conflicts, and adhere to coding style to maintain software quality
+            </li>
+          </ul>
         </div>
 
         <div className="ml-4 mt-4">
@@ -199,23 +223,28 @@ export default function Home() {
             MERN Stack Website
           </h2>
           <p className="m-0 text-sm opacity-50">
+
+            MERN Stack Front To Back, Udemy Course
+
+            <br></br>
+            <br></br>
+
             placeholder description.....Following this is some placeholder text... What is Lorem Ipsum?
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
           </p>
 
-
-            <h3 className="mt-2">Key Responsibilities</h3>
-            <ul className="ml-8 mt-2 list-disc">
-              <li>
-                list item 1
-              </li>
-              <li>
-                list item 2
-              </li>
-              <li>
-                list item 3
-              </li>
-            </ul>
+          <h3 className="mt-2">Key Responsibilities</h3>
+          <ul className="ml-8 mt-2 list-disc">
+            <li>
+              Developed using Express.js and Mongoose.js interacting with MongoDB to create back-end of a web application
+            </li>
+            <li>
+              Utilised Postman to test HTTP request functionality for the back-end
+            </li>
+            <li>
+              Developed front-end of application using React.js, Redux.js, and Axios to create a dynamic UI
+            </li>
+          </ul>
         </div>
       </section>
 
